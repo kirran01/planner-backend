7b933b90abc05452af151c20f53e52cf0edbd723
kirran01 <Kirran-@live.com>
2022-12-15 17:12:50 -0500

    hashed passwords for creating users on signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,33 @@
+const bcryptjs = require("bcryptjs");
+const User = require("../models/User.model");
+
 const loginController = (req, res) => {
-  res.send("pizza!");
+  res.send("loginroute hit!");
 };
 
 const signupController = (req, res) => {
-  res.send("cookies!");
+  const { email, password, name } = req.body;
+  if (!req.body.email || !req.body.password || !req.body.name) {
+    return res.status(400).json({
+      error: {
+        message: "field(s) are missing",
+      },
+    });
+  }
+  bcryptjs.hash(password, 10)
+  .then((hashedPassword) => {
+    return User.create({
+      email,
+      name,
+      password: hashedPassword,
+    })
+      .then((createdUser) => {
+        res.send(createdUser);
+      })
+      .catch((err) => {
+        res.send(err,"err");
+      });
+  });
 };
 
 module.exports = { loginController, signupController };
