54604799e5777907a6e6bd7b624586eeb235092f
kirran01 <Kirran-@live.com>
2022-12-15 17:47:45 -0500

    users get auth tokens upon login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,50 @@
 const bcryptjs = require("bcryptjs");
 const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
 
 const loginController = (req, res) => {
-  res.send("loginroute hit!");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.json({
+      error: {
+        message: "field(s) are blank",
+      },
+    });
+  }
+  let myUser;
+  User.findOne({
+    email,
+  })
+    .then((foundUser) => {
+      if (!foundUser) {
+        return Promise.reject("invalid email or password");
+      }
+      myUser = foundUser;
+      console.log(foundUser);
+      return bcryptjs.compare(password, foundUser.password);
+    })
+    .then((isValidPassword) => {
+      if (!isValidPassword) {
+        return Promise.reject("invalid email or password");
+      }
+      const payload = {
+        _id: myUser._id,
+        name: myUser.name,
+        email: myUser.email,
+      };
+      console.log(payload);
+      const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+        algorithm: "HS256",
+        expiresIn: "72h",
+      });
+      res.json({
+        authToken: authToken,
+        hi: "hello",
+      });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 const signupController = (req, res) => {
@@ -14,8 +56,7 @@ const signupController = (req, res) => {
       },
     });
   }
-  bcryptjs.hash(password, 10)
-  .then((hashedPassword) => {
+  bcryptjs.hash(password, 10).then((hashedPassword) => {
     return User.create({
       email,
       name,
@@ -25,7 +66,7 @@ const signupController = (req, res) => {
         res.send(createdUser);
       })
       .catch((err) => {
-        res.send(err,"err");
+        res.send(err, "err");
       });
   });
 };
