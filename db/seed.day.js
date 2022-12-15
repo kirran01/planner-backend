3187fad2d412406f57f08cc175b121e2459c1e52
kirran01 <Kirran-@live.com>
2022-12-15 15:41:26 -0500

    day is seeding with refrence to events

--- db/seed.day.js
+++ db/seed.day.js
@@ -1,18 +1,36 @@
 require("dotenv/config");
 const Day = require("../models/Day.model");
+const Event = require("../models/Event.model");
 const mongoose = require("mongoose");
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then((connectionRes) => {
     console.log("connected to -->", connectionRes.connections[0].name);
+    return Event.create({
+      name: "testforday",
+      eventUrl: "testforday",
+      imageUrl: "testforday",
+      userEntry: "testforday",
+    });
+  })
+  .then((createdEvent) => {
+    console.log("created event for day test --->", createdEvent);
     return Day.create({
       day: Date.now(),
+      events: createdEvent._id,
     });
   })
   .then((createdDay) => {
-    console.log("created day ---->", createdDay);
+    console.log("created Day--->", createdDay);
+    return Day.findByIdAndUpdate(createdDay.events, {
+      $push: {
+        events: createdDay._id,
+      },
+      new: true,
+    });
   })
+  .then()
   .catch((err) => {
     console.log("err--->", err);
   });
