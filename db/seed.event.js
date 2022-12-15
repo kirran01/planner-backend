39d5fcb5054c7f27a490973204164094415a05bc
kirran01 <Kirran-@live.com>
2022-12-15 14:45:58 -0500

    events seeding

--- db/seed.event.js
+++ db/seed.event.js
@@ -6,6 +6,15 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then((mongooseRes) => {
     console.log("connected to -->", mongooseRes.connections[0].name);
+    return Event.create({
+      name: "event1",
+      imageUrl: "event1",
+      eventUrl: "event1",
+      userEntry: "event1",
+    });
+  })
+  .then((createdEvent) => {
+    console.log("created event-->", createdEvent);
   })
   .catch((err) => {
     console.log(err, "<--err");
