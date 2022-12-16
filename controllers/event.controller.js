215c24cb10d7025f02d5500c17ba9a267e3bf861
kirran01 <Kirran-@live.com>
2022-12-16 16:47:31 -0500

    create event route with controller

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -1,6 +1,22 @@
 const Event = require("../models/Event.model");
 const Day = require("../models/Day.model");
 
+const createEventController = (req, res) => {
+  //   res.send("post route hit for event");
+  Event.create({
+    name: req.body.name,
+    eventUrl: req.body.eventUrl,
+    imageUrl: req.body.imageUrl,
+    userEntry: req.body.userEntry,
+  })
+    .then((createdEvent) => {
+      res.send(createdEvent);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+
 const getAllEventsController = (req, res) => {
   Event.find()
     .then((foundEvents) => {
@@ -41,8 +57,14 @@ const updateEventController = (req, res) => {
     });
 };
 
+const deleteEventController = (req, res) => {
+  res.send(`delete route for event hit @ ${req.params.id}`);
+};
+
 module.exports = {
   getAllEventsController,
   getEventByIdController,
   updateEventController,
+  deleteEventController,
+  createEventController,
 };
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Event = require("../models/Event.model");
 const {
+  createEventController,
   getAllEventsController,
   getEventByIdController,
   updateEventController,
+  deleteEventController,
 } = require("../controllers/event.controller");
 
+router.post("/create-event", createEventController);
 router.get("/all", getAllEventsController);
 router.get("/all/:id", getEventByIdController);
 router.put("/all/:id", updateEventController);
+router.delete("/all/:id", deleteEventController);
 
 module.exports = router;
