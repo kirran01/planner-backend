5098fec4d2f556c83a7baf154d1d1d261ae41f94
kirran01 <Kirran-@live.com>
2022-12-21 17:38:41 -0500

    safety commit

--- models/Day.model.js
+++ models/Day.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const daySchema = new Schema({
   day: Date,
   quote: String,
-  myEvents: [{ type: Schema.Types.ObjectId, ref: "Event" }],
+  myEvents: [{ type: Schema.Types.ObjectId, ref: "Event" }]
 });
 
 const Day = mongoose.model("Day", daySchema);
